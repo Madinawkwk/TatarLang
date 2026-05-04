708a7fb04fee2d845ec0a3b86de65f60295491cd
Madinawkwk <msh.ajj@bk.ru>
2026-05-04 14:36:12 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def main_page():
+    return render_template('index.html')
+
+@app.route('/first_lesson_page')
+def first_lesson():
+    return render_template('firstlesson.html')
+
+@app.route('/second_lesson_page')
+def second_lesson():
+    return render_template('secondlesson.html')
+
+@app.route('/third_lesson_page')
+def third_lesson():
+    return render_template('thirdlesson.html')
+
+@app.route('/fourth_lesson_page')
+def fourth_lesson():
+    return render_template('fourthlesson.html')
+
+@app.route('/fifth_lesson_page')
+def fifth_lesson():
+    return render_template('fifthlesson.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
